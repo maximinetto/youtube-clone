b48f35ce0424bb0e50d33c4281de565f5673d196
Maximiliano Minetto <maximinetto@gmail.com>
2021-11-06 12:16:47 -0300

    add video layout

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import "./_index.module.scss";
+import styles from "./_index.module.scss";
 
 function Video() {
-  return <div>Video</div>;
+  return (
+    <div className={styles.video}>
+      <div className={styles.videoTop}>
+        <img
+          src="https://i.ytimg.com/an_webp/V84yjOdtybQ/mqdefault_6s.webp?du=3000&sqp=CKjBiowG&rs=AOn4CLA_MzIWUuiULZeCVrjeBO_rdXTTkw"
+          alt=""
+        />
+        <span>05:43</span>
+      </div>
+      <div className={styles.videoTitle}>
+        How to create a app in 5 minutes #made by Maximinetto
+      </div>
+      <div className={styles.videoChannel}>
+        <img
+          src="https://yt3.ggpht.com/ytc/AKedOLSsnWm_dQzIqM-qgW74yebXNX_b__k6WAeUBb6GeGQ=s176-c-k-c0x00ffffff-no-rj-mo"
+          alt="Photo"
+        />
+        <p>Maximinetto</p>
+      </div>
+      <div className={styles.videoDetails}>
+        <span>5m views •</span>
+        <span>5 days ago</span>
+      </div>
+    </div>
+  );
 }
 
 export default Video;
